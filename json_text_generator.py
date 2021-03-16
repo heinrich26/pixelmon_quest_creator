6721fee7bc859ee68c51cbc330bc8e1fdaf7d091
heinrich26 <65970327+heinrich26@users.noreply.github.com>
2021-03-16 14:09:56 +0100

    more edits

--- json_text_generator.py
+++ json_text_generator.py
@@ -1,17 +1,13 @@
 import platform
-import urllib.request
 from tkinter import *
 from tkinter import ttk
 from tkinter.font import Font
 from tkinter import font
+import random
 import pyglet
 from tkinterhtml import HtmlFrame
-from tkhtmlview import HTMLScrolledText
-import ssl
 
 # This restores the same behavior as before.
-context = ssl._create_unverified_context()
-
 
 current_os = platform.system()
 
@@ -21,21 +17,21 @@ if current_os == "Windows" or current_os == "Darwin":
 
 formatting_codes = {
     "§0": "\"background:#fff;color:#000000;\"",
-    "§1": "\"background:#000;color: #0000AA;\"",
-    "§2": "\"background:#000;color: #00AA00;\"",
-    "§3": "\"background:#000;color: #00AAAA;\"",
-    "§4": "\"background:#000;color: #AA0000;\"",
-    "§5": "\"background:#000;color: #AA00AA;\"",
-    "§6": "\"background:#000;color: #FFAA00;\"",
-    "§7": "\"background:#000;color: #AAAAAA;\"",
-    "§8": "\"background:#000;color: #555555;\"",
-    "§9": "\"background:#000;color: #5555FF;\"",
-    "§a": "\"background:#000;color: #55FF55;\"",
-    "§b": "\"background:#000;color: #55FFFF;\"",
+    "§1": "\"background:#000;color:#0000AA;\"",
+    "§2": "\"background:#000;color:#00AA00;\"",
+    "§3": "\"background:#000;color:#00AAAA;\"",
+    "§4": "\"background:#000;color:#AA0000;\"",
+    "§5": "\"background:#000;color:#AA00AA;\"",
+    "§6": "\"background:#000;color:#FFAA00;\"",
+    "§7": "\"background:#000;color:#AAAAAA;\"",
+    "§8": "\"background:#000;color:#555555;\"",
+    "§9": "\"background:#000;color:#5555FF;\"",
+    "§a": "\"background:#000;color:#55FF55;\"",
+    "§b": "\"background:#000;color:#55FFFF;\"",
     "§c": "\"background:#000;color:#FF5555;\"",
-    "§d": "\"background:#000;color: #FF55FF;\"",
-    "§e": "\"background:#000;color: #FFFF55;\"",
-    "§f": "\"background:#000;color: #ffffff;\"",
+    "§d": "\"background:#000;color:#FF55FF;\"",
+    "§e": "\"background:#000;color:#FFFF55;\"",
+    "§f": "\"background:#000;color:#ffffff;\"",
     "§k": "\"font-family:Wingdings;\"",
     "§l": "\"font-weight: bold;\"",
     "§m": "\"text-decoration: line-through;\"",
@@ -67,7 +63,7 @@ formatting_codes = {
 #     "§o"
 # ]
 #
-formatting_names = [ "black","dark_blue","dark_green","dark_aqua","dark_red","dark_purple","gold","gray","dark_gray","blue","green","aqua","red","light_purple","yellow","white","obfuscated","bold","striketrough","underline","italic" ]
+formatting_names = [ "black","dark_blue","dark_green","dark_aqua","dark_red","dark_purple","gold","gray","dark_gray","blue","green","aqua","red","light_purple","yellow","white","obfuscated","bold","striketrough","underline","italic","reset", "New Line" ]
 
 def find_all(str, sub):
     start = 0
@@ -78,6 +74,48 @@ def find_all(str, sub):
         start += len(sub)
 
 
+
+
+alphabet = [
+"i,;.:!|î", # 1px
+"l'`Ììí·´", # 2px
+"It[]ÍÎÏïªº•°", #3px
+"""kf(){}*¤²”\"""", # 4px
+"""ABCDEFGHJKLMNOPQRSTUVWXYZabcdeghjmnopqrsuvwxyz/?$%&+-#_¯=^¨£ÀÁÂÃÄÅÇÈÉÊËÑÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëñðòóôõöùúûüýÿ0123456789Ææß×¼½¿¬«»""", # 5px
+"~@®÷±"]
+
+class animate_obfuscated_text(object):
+    def __init__(self, parent, var, text):
+        self.parent = parent
+        self.var = var
+        self.text = text
+        self.new_text = "".join([self.randchar(char) for char in self.text])
+        self.var.set(self.text)
+        self.parent.after(500, self.refresh)
+
+
+    def randchar(self, i):
+        global alphabet
+        if i == " ":
+            return " "
+        elif i in alphabet[0]:
+            return alphabet[0][random.randrange(0,7,1)]
+        elif i in alphabet[1]:
+            return alphabet[1][random.randrange(0,7,1)]
+        elif i in alphabet[2]:
+            return alphabet[2][random.randrange(0,11,1)]
+        elif i in alphabet[3]:
+            return alphabet[3][random.randrange(0,10,1)]
+        elif i in alphabet[4]:
+            return alphabet[4][random.randrange(0, len(alphabet[4])-1,1)]
+        elif i in alphabet[5]:
+            return alphabet[5][random.randrange(0,4,1)]
+        else:
+            return i
+
+    def refresh(self):
+        self.var.set("".join([self.randchar(char) for char in self.text]))
+        self.parent.after(100, self.refresh)
 
 
 class JSON_text_Generator(object):
@@ -93,9 +131,11 @@ class JSON_text_Generator(object):
             json_frame.title("Configure JSON Text")
             json_frame.grab_set()
             json_frame.maxsize(500, 200)
+        self.user_input=""
         json_frame.columnconfigure(0, weight=1, minsize=300)
         ttk.Label(json_frame, text="Enter formatted Text:").grid(row=0, column=0, sticky=W, pady=4, padx=4)
         self.text_field = Text(json_frame, font=("Minecraft Regular", 16), height=6, width=30, wrap=WORD)
+        self.text_field.insert(1.0, self.user_input)
         self.text_field.grid(row=1, column=0, padx=(4,0), sticky=W+E)
         self.text_field.bind("<KeyRelease>", self.update_prev_text)
         self.text_scrollbar = ttk.Scrollbar(json_frame, orient=VERTICAL, command=self.text_field.yview)
@@ -106,42 +146,81 @@ class JSON_text_Generator(object):
         json_frame.rowconfigure(3, weight=1)
         limiter  = ttk.Frame(json_frame, width=10, height=4)
         limiter.grid(row=3, column=0, padx=4, pady=(0,4), columnspan=2)
-        self.prev_field = HtmlFrame(limiter, takefocus=0)
+        self.prev_field = HtmlFrame(limiter, takefocus=0, relief="sunken", borderwidth=1)
         self.prev_field.pack(pady=(0,4))
 
-        formatting_info = ttk.Treeview(json_frame, columns=("name"))
-        formatting_info.grid(row=1, column=2, padx=4, rowspan=3, sticky=N+S)
-        formatting_info.column("#0", width=100, anchor=CENTER)
-        formatting_info.heading('#0', text="Formatting Code")
-        formatting_info.heading("name", text="Name")
-        formatting_info.column("name", width=170)
+        self.formatting_info = Frame(json_frame, bg="#a2a9b1")
+        self.formatting_info.grid(row=0, column=2, padx=4, rowspan=4, sticky=N, pady=(4,0))
+        ttk.Label(self.formatting_info, font=("Minecraft Regular", 9), text="Code").grid(row=0, column=0, sticky=W+E, pady=1, padx=1)
+        ttk.Label(self.formatting_info, font=("Minecraft Regular", 9), text="Name", anchor="c").grid(row=0, column=1, sticky=W+E, pady=1, padx=(0,1))
 
-        [formatting_info.insert("", END, key, text=key, values=(formatting_names[list(formatting_codes.keys()).index(key)]), tags=(formatting_names[list(formatting_codes.keys()).index(key)])) for key in list(formatting_codes.keys())]
-        formatting_info.insert("", 21, "§r", text="§r", values=("reset"), tags="black")
-        self.obfuscated = PhotoImage(file="src/obfuscated.gif")
-        formatting_info.set("§k","§k")
+        forminfo_values = 1
+        for key in list(formatting_codes.keys()):
+            if forminfo_values <= 15:
+                if forminfo_values in (1,2,3,4,5,6,9,10):
+                    ttk.Label(self.formatting_info, text=key, anchor="c", font=("Minecraft Regular", 9), foreground="#fff",background=formatting_codes[key][23:30]).grid(row=forminfo_values, column=0, sticky=W+E, pady=(0,1), padx=1)
+                else:
+                    ttk.Label(self.formatting_info, text=key, anchor="c", font=("Minecraft Regular", 9), background=formatting_codes[key][23:30]).grid(row=forminfo_values, column=0, sticky=W+E, pady=(0,1), padx=1)
+            elif forminfo_values <= 21:
+                ttk.Label(self.formatting_info, anchor="c", background="white", font=("Minecraft Regular", 9), text=key).grid(row=forminfo_values, column=0, sticky=W+E, pady=(0,1), padx=1)
+            forminfo_values += 1
+        ttk.Label(self.formatting_info, background="white", anchor="c", font=("Minecraft Regular", 9), text="§r").grid(row=22, column=0, sticky=W+E, pady=(0,1), padx=1)
+        ttk.Label(self.formatting_info, background="white", anchor="c", font=("Minecraft Regular", 9), text="\\n").grid(row=23, column=0, sticky=W+E, pady=(0,1), padx=1)
+        for i in range(0, 23):
+            if i <= 14:
+                if i in (0,1,2,3,4,5,8,9):
+                    ttk.Label(self.formatting_info, text=formatting_names[i], font=("Minecraft Regular", 9), anchor="c", foreground="#fff",background=list(formatting_codes.values())[i][23:30]).grid(row=i+1, column=1, sticky=W+E, pady=(0,1), padx=(0,1))
+                else:
+                    ttk.Label(self.formatting_info, text=formatting_names[i], font=("Minecraft Regular", 9), anchor="c", background=list(formatting_codes.values())[i][23:30]).grid(row=i+1, column=1, sticky=W+E, pady=(0,1), padx=(0,1))
+            elif i in (15,21,22):
+                ttk.Label(self.formatting_info, anchor="c", background="white", font=("Minecraft Regular", 9), text=formatting_names[i]).grid(row=i+1, column=1, sticky=W+E, pady=(0,1), padx=(0,1))
 
-        formatting_info.tag_configure("red", foreground="#FF5555", font=("Minecraft Regular", 10))
-        formatting_info.tag_configure("blue", foreground="#5555FF", font=("Minecraft Regular", 10))
-        formatting_info.tag_configure("green", foreground="#55FF55", font=("Minecraft Regular", 10))
-        formatting_info.tag_configure("dark_blue", foreground="#0000AA", font=("Minecraft Regular", 10))
-        formatting_info.tag_configure("dark_aqua", foreground="#00AAAA", font=("Minecraft Regular", 10))
-        formatting_info.tag_configure("white", foreground="#ffffff", background="#AAAAAA", font=("Minecraft Regular", 10))
-        formatting_info.tag_configure("black", foreground="#000000", font=("Minecraft Regular", 10))
-        formatting_info.tag_configure("dark_gray", foreground="#555555", font=("Minecraft Regular", 10))
-        formatting_info.tag_configure("gray", foreground="#AAAAAA", font=("Minecraft Regular", 10))
-        formatting_info.tag_configure("dark_purple", foreground="#AA00AA", font=("Minecraft Regular", 10))
-        formatting_info.tag_configure("light_purple", foreground="#FF55FF", font=("Minecraft Regular", 10))
-        formatting_info.tag_configure("dark_red", foreground="#AA0000", font=("Minecraft Regular", 10))
-        formatting_info.tag_configure("yellow", foreground="#FFFF55", background="#AAAAAA", font=("Minecraft Regular", 10))
-        formatting_info.tag_configure("gold", foreground="#FFAA00", font=("Minecraft Regular", 10))
-        formatting_info.tag_configure("aqua", foreground="#55FFFF", font=("Minecraft Regular", 10))
-        formatting_info.tag_configure("dark_green", foreground="#00AA00", font=("Minecraft Regular", 10))
-        formatting_info.tag_configure("bold", font=("Minecraft Regular", 10, "bold"))
-        formatting_info.tag_configure("italic", font=("Minecraft Regular", 10, "italic"))
-        formatting_info.tag_configure("obfuscated", foreground="#000000",font=("Minecraft Regular", 10), image=self.obfuscated)
-        formatting_info.tag_configure("underline", font=("Minecraft Regular", 10, "underline"))
-        formatting_info.tag_configure("striketrough", font=("Minecraft Regular", 10, "overstrike"))
+        self.obfuscated_text = StringVar()
+        # self.obfuscated_image_cnt =
+        # self.obfuscated_image = [PhotoImage(file='src/obfuscated.gif',format = 'gif -index %i' %(i)) for i in range(self.obfuscated_image_cnt)]
+        self.obfuscated_example = ttk.Label(self.formatting_info, background="white", anchor="c", font=("Minecraft Regular", 9), width=10, textvariable=self.obfuscated_text, foreground="#000")
+        self.obfuscated_example.grid(row=17, column=1, sticky=W+E, pady=(0,1), padx=(0,1))
+
+        ttk.Label(self.formatting_info, background="white", anchor="c", font=("Minecraft Regular", 9, "bold"), text=formatting_names[17], foreground="#000").grid(row=18, column=1, sticky=W+E, pady=(0,1), padx=(0,1))
+        ttk.Label(self.formatting_info, background="white", anchor="c", font=("Minecraft Regular", 9, "overstrike"), text=formatting_names[18], foreground="#000").grid(row=19, column=1, sticky=W+E, pady=(0,1), padx=(0,1))
+        ttk.Label(self.formatting_info, background="white", anchor="c", font=("Minecraft Regular", 9, "underline"), text=formatting_names[19], foreground="#000").grid(row=20, column=1, sticky=W+E, pady=(0,1), padx=(0,1))
+        ttk.Label(self.formatting_info, background="white", anchor="c", font=("Minecraft Regular", 9, "italic"), text=formatting_names[20], foreground="#000").grid(row=21, column=1, sticky=W+E, pady=(0,1), padx=(0,1))
+
+        # formatting_info.insert("", 21, "§r", text="§r", values=("reset"), tags="black")
+        # formatting_info.set("§k","§k")
+        #
+        # formatting_info.tag_configure("red", foreground="#FF5555", font=("Minecraft Regular", 10))
+        # formatting_info.tag_configure("blue", foreground="#5555FF", font=("Minecraft Regular", 10))
+        # formatting_info.tag_configure("green", foreground="#55FF55", font=("Minecraft Regular", 10))
+        # formatting_info.tag_configure("dark_blue", foreground="#0000AA", font=("Minecraft Regular", 10))
+        # formatting_info.tag_configure("dark_aqua", foreground="#00AAAA", font=("Minecraft Regular", 10))
+        # formatting_info.tag_configure("white", foreground="#ffffff", background="#AAAAAA", font=("Minecraft Regular", 10))
+        # formatting_info.tag_configure("black", foreground="#000000", font=("Minecraft Regular", 10))
+        # formatting_info.tag_configure("dark_gray", foreground="#555555", font=("Minecraft Regular", 10))
+        # formatting_info.tag_configure("gray", foreground="#AAAAAA", font=("Minecraft Regular", 10))
+        # formatting_info.tag_configure("dark_purple", foreground="#AA00AA", font=("Minecraft Regular", 10))
+        # formatting_info.tag_configure("light_purple", foreground="#FF55FF", font=("Minecraft Regular", 10))
+        # formatting_info.tag_configure("dark_red", foreground="#AA0000", font=("Minecraft Regular", 10))
+        # formatting_info.tag_configure("yellow", foreground="#FFFF55", background="#AAAAAA", font=("Minecraft Regular", 10))
+        # formatting_info.tag_configure("gold", foreground="#FFAA00", font=("Minecraft Regular", 10))
+        # formatting_info.tag_configure("aqua", foreground="#55FFFF", font=("Minecraft Regular", 10))
+        # formatting_info.tag_configure("dark_green", foreground="#00AA00", font=("Minecraft Regular", 10))
+        # formatting_info.tag_configure("bold", font=("Minecraft Regular", 10, "bold"))
+        # formatting_info.tag_configure("italic", font=("Minecraft Regular", 10, "italic"))
+        # formatting_info.tag_configure("obfuscated", foreground="#000000",font=("Minecraft Regular", 10), image=self.obfuscated)
+        # formatting_info.tag_configure("underline", font=("Minecraft Regular", 10, "underline"))
+        # formatting_info.tag_configure("striketrough", font=("Minecraft Regular", 10, "overstrike"))
+        anim_text = animate_obfuscated_text(root, self.obfuscated_text, "Obfuscated")
+        ttk.Button(json_frame, text="Save", command=self.save_input).grid(row=4, column=3)
+
+    def save_input(self):
+        self.user_input=self.text_field.get(1.0,END)[:-1].replace("\n", "\\n").replace("§", """\\u00A7""")
+        while self.user_input.endswith("\\n"):
+            self.user_input = self.user_input[:-2]
+
+    def encode_string(self):
+        self.output_string = "".join(["\\u0000"[0:6-len(str(ord(char)))] + str(ord(char)) if not char in """ABCDEFGHJKLMNOPQRSTUVWXYZabcdeghjmnopqrsuvwxyz1234567890,.!/()[]{}?\+*"'#-_<>|%=""" else char for char in self.user_input.replace('\"', '\\"').replace("\'", "\\'")])
+        print(self.output_string)
 
 
     def update_prev_text(self, e):
@@ -156,16 +235,15 @@ class JSON_text_Generator(object):
         spans_to_close = formatted_string.count("<span style=") - formatted_string.count("</span>")
         spans = []
         formatted_string += "".join("</span>" for i in range(0, spans_to_close))
-        print(formatted_string, spans_to_close)
 
         html = """<html><body style="background:#000;word-warp:break-word;font-family:minecraft_font;font-size:26px;color:#fff;width:500px;">""" + str(formatted_string) + "</body></html>"
         self.prev_field.set_content(html)
 
         #self.prev_field.set_html(html)
         # print(user_input, type(user_input))
-        formatting_info.delete(0.0,END)
+        # formatting_info.delete(0.0,END)
         # [self.prev_field.mark_unset(mark) for mark in self.prev_field.mark_names()[2:]]
-        formatting_info.insert(0.0, user_input)
+        # formatting_info.insert(0.0, user_input)
         # replacements = 0
         # for x in formatting_codes:
         #     counter = 0
@@ -191,7 +269,7 @@ if __name__ == "__main__":
 
     root = Tk()
     root.title("JSON Text Generator")
-    root.maxsize(1000000000, 500)
+    root.maxsize(800, 561)
     mcfont = font.Font(name="mc font", family="mc font", font=("Minecraft Regular", 16))
 
     app = JSON_text_Generator()
